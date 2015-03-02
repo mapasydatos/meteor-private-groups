a0c11fedd4f314809d31f08e4cfb2d37bd753816
Kestanous <CMTopus@me.com>
2015-03-02 17:36:02 -0500

    use fixed candid

--- package.js
+++ package.js
@@ -16,7 +16,7 @@ Package.onUse(function(api) {
   api.use([
     'meteor-platform', //we use all core standards
     'alanning:roles@1.2.13', 
-    'kestanous:candid'
+    'kestanous:candid@0.3.0-dev'
   ])
 
   api.addFiles('private:groups.js');
